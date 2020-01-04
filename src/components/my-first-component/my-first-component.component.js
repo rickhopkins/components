a87191bad23251e7551f788a22cd4ff4792f7f8d
Rick Hopkins <rhopkins@nhaschools.com>
2020-01-04 17:03:47 -0500

    Added test data and made an ajax call

--- src/components/my-first-component/my-first-component.component.js
+++ src/components/my-first-component/my-first-component.component.js
@@ -1,5 +1,6 @@
 import { ComponentBase, injector } from '../../test.module.js';
 import { myFirstComponentTemplate } from './my-first-component.template.js';
+import { HttpGet } from '../../http/http-get.class.js';
 
 export class MyFirstComponent extends ComponentBase {
 	static tag = 'my-first-component';
@@ -18,6 +19,13 @@ export class MyFirstComponent extends ComponentBase {
 
 		this.template = () => myFirstComponentTemplate(this.tag, this.testAttr1, this.testAttr2, test.getValues(), test.isTrue);
 		this.render();
+
+		const reqTopic = new HttpGet('/data/users.json').send();
+		reqTopic.subscribe((users) => {
+			console.log(users);
+		});
+
+		setTimeout(() => reqTopic.next([]), 5000);
 	}
 }
 
